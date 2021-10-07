5568f536c1ce6a8ab9c557a650ef00e2aca9ec96
Julio Fernandez <jfernandez74@gmail.com>
2021-10-07 08:35:55 +0200

    Add launch data to view command

--- src/main/java/io/seqera/tower/cli/responses/actions/ActionsView.java
+++ src/main/java/io/seqera/tower/cli/responses/actions/ActionsView.java
@@ -2,9 +2,13 @@ package io.seqera.tower.cli.responses.actions;
 
 import java.io.PrintWriter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.seqera.tower.JSON;
 import io.seqera.tower.cli.responses.Response;
+import io.seqera.tower.cli.utils.ModelHelper;
 import io.seqera.tower.cli.utils.TableList;
 import io.seqera.tower.model.Action;
+import io.seqera.tower.model.WorkflowLaunchRequest;
 
 public class ActionsView extends Response {
 
@@ -16,6 +20,14 @@ public class ActionsView extends Response {
 
     @Override
     public void toString(PrintWriter out) {
+        String configJson = "";
+        try {
+            WorkflowLaunchRequest request = ModelHelper.createLaunchRequest(action.getLaunch());
+            configJson = new JSON().getContext(WorkflowLaunchRequest.class).writerWithDefaultPrettyPrinter().writeValueAsString(request);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
         out.println(ansi(String.format("%n  @|bold Details for action '%s'|@%n", action.getName())));
 
         TableList table = new TableList(out, 2);
@@ -24,10 +36,13 @@ public class ActionsView extends Response {
         table.addRow("Name", action.getName());
         table.addRow("Pipeline URL", action.getLaunch().getPipeline());
         table.addRow("Source", action.getSource().toString());
-        table.addRow("Status", action.getStatus().toString());
+        table.addRow("Hook URL", action.getHookUrl());
+        table.addRow("Last event", formatTime(action.getLastSeen()));
         table.addRow("Date created", formatTime(action.getDateCreated()));
         table.addRow("Last event", formatTime(action.getLastSeen()));
         table.print();
-        out.println("");
+
+        out.println(String.format("%n  Configuration:%n%n%s%n", configJson.replaceAll("(?m)^", "     ")));
+
     }
 }
