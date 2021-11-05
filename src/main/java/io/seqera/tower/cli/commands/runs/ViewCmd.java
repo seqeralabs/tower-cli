32d2ea2935477e5392708e612282e1d4d6b44b31
Julio Fernandez <jfernandez74@gmail.com>
2021-11-05 08:56:24 +0100

    Improve run view subcommand

--- src/main/java/io/seqera/tower/cli/commands/runs/ViewCmd.java
+++ src/main/java/io/seqera/tower/cli/commands/runs/ViewCmd.java
@@ -18,9 +18,17 @@ import io.seqera.tower.cli.responses.Response;
 import io.seqera.tower.cli.responses.RunView;
 import io.seqera.tower.model.ComputeEnv;
 import io.seqera.tower.model.Launch;
+import io.seqera.tower.model.ProgressData;
 import io.seqera.tower.model.Workflow;
 import io.seqera.tower.model.WorkflowLoad;
+import io.seqera.tower.model.WorkflowMetrics;
 import picocli.CommandLine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @CommandLine.Command(
         name = "view",
@@ -28,22 +36,170 @@ import picocli.CommandLine;
 )
 public class ViewCmd extends AbstractRunsCmd {
 
-    @CommandLine.Option(names = {"-i", "--id"}, description = "Pipeline's run identifier", required = true)
-    public String id;
+    @CommandLine.Mixin
+    public RunViewOptions opts;
 
     @CommandLine.Mixin
     public WorkspaceOptions workspace;
 
+    @CommandLine.Option(names = {"-i", "--id"}, description = "Pipeline's run identifier", required = true)
+    public String id;
+
     protected Response exec() throws ApiException {
         try {
+            String workspaceRef = workspaceRef(workspace.workspaceId);
             Workflow workflow = workflowById(workspace.workspaceId, id);
-
-            Launch launch = launchById(workspace.workspaceId, workflow.getLaunchId());
-
             WorkflowLoad workflowLoad = workflowLoadByWorkflowId(workspace.workspaceId, id);
+            Launch launch = launchById(workspace.workspaceId, workflow.getLaunchId());
             ComputeEnv computeEnv = launch.getComputeEnv();
 
-            return new RunView(id, workspaceRef(workspace.workspaceId), workflow, workflowLoad, computeEnv);
+            ProgressData progress = null;
+            if (opts.processes || opts.stats || opts.load || opts.utilization) {
+                progress = api().describeWorkflowProgress(id, workspace.workspaceId).getProgress();
+            }
+
+            List<WorkflowMetrics> metrics = null;
+            if (opts.metricsCpu || opts.metricsMemory || opts.metricsIo || opts.metricsTime) {
+                metrics = api().describeWorkflowMetrics(id, workspace.workspaceId).getMetrics();
+            }
+
+            Map<String, Object> general = new HashMap<String, Object>();
+            general.put("id", workflow.getId());
+            general.put("runName", workflow.getRunName());
+            general.put("startingDate", workflow.getStart());
+            general.put("commitId", workflow.getCommitId());
+            general.put("sessionId", workflow.getSessionId());
+            general.put("username", workflow.getUserName());
+            general.put("workdir", workflow.getWorkDir());
+            general.put("container", workflow.getContainer());
+            general.put("executors", workflowLoad.getExecutors() != null ? String.join(", ", workflowLoad.getExecutors()) : null);
+            general.put("computeEnv", computeEnv.getName());
+            general.put("nextflowVersion", workflow.getNextflow() != null ? workflow.getNextflow().getVersion() : null);
+
+            Map<String, Object> config = new HashMap<String, Object>();
+            if (opts.config) {
+                config = workflow.getParams();
+            }
+
+            Map<String, Object> params = new HashMap<String, Object>();
+            if (opts.params) {
+                params = workflow.getParams();
+            }
+
+            String command = null;
+            if (opts.command) {
+                command = workflow.getCommandLine();
+            }
+
+            Map<String, Object> status = new HashMap<>();
+            if (opts.status) {
+                status.put("pending", workflowLoad.getPending());
+                status.put("submitted", workflowLoad.getSubmitted());
+                status.put("running", workflowLoad.getRunning());
+                status.put("cached", workflowLoad.getCached());
+                status.put("succeeded", workflowLoad.getSucceeded());
+                status.put("failed", workflowLoad.getFailed());
+            }
+
+            final List<Map<String, Object>> processes = new ArrayList<>();
+            if (opts.processes) {
+                progress.getProcessesProgress().forEach(it -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("name", it.getProcess());
+                    data.put("completedTasks", it.getSucceeded() + it.getCached());
+                    data.put("totalTasks", it.getPending() + it.getRunning() + it.getCached() + it.getSubmitted() + it.getSucceeded() + it.getFailed());
+
+                    processes.add(data);
+                });
+            }
+
+            Map<String, Object> stats = new HashMap<>();
+            if (opts.stats) {
+                stats.put("wallTime", TimeUnit.MILLISECONDS.toSeconds(workflow.getDuration()) / 60D);
+                stats.put("cpuTime", TimeUnit.MILLISECONDS.toMinutes(progress.getWorkflowProgress().getCpuTime()) / 60D);
+                stats.put("totalMemory", progress.getWorkflowProgress().getMemoryRss() / 1024 / 1024 / 1024D);
+                stats.put("read", progress.getWorkflowProgress().getReadBytes() / 1024 / 1024 / 1024D);
+                stats.put("write", progress.getWorkflowProgress().getWriteBytes() / 1024 / 1024 / 1024D);
+                stats.put("cost", progress.getWorkflowProgress().getCost());
+            }
+
+            Map<String, Object> load = new HashMap<>();
+            if (opts.load) {
+                load.put("peakCpus", progress.getWorkflowProgress().getPeakCpus());
+                load.put("loadCpus", progress.getWorkflowProgress().getLoadCpus());
+                load.put("peakTasks", progress.getWorkflowProgress().getPeakTasks());
+                load.put("loadTasks", progress.getWorkflowProgress().getLoadTasks());
+            }
+
+            Map<String, Object> utilization = new HashMap<>();
+            if (opts.utilization) {
+                utilization.put("memoryEfficiency", progress.getWorkflowProgress().getMemoryEfficiency());
+                utilization.put("cpuEfficiency", progress.getWorkflowProgress().getCpuEfficiency());
+            }
+
+            List<Map<String, Object>> metricsMem = new ArrayList<>();
+            if (opts.metricsMemory) {
+                metrics.forEach(it -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("process", it.getProcess());
+                    data.put("memRaw", it.getMem());
+                    data.put("memUsage", it.getMemUsage());
+                    data.put("memVirtual", it.getVmem());
+
+                    metricsMem.add(data);
+                });
+            }
+
+            List<Map<String, Object>> metricsCpu = new ArrayList<>();
+            if (opts.metricsCpu) {
+                metrics.forEach(it -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("process", it.getProcess());
+                    data.put("cpuRaq", it.getCpu());
+                    data.put("cpuUsage", it.getCpuUsage());
+
+                    metricsCpu.add(data);
+                });
+            }
+
+            List<Map<String, Object>> metricsTime = new ArrayList<>();
+            if (opts.metricsTime) {
+                metrics.forEach(it -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("timeRaw", it.getTime());
+                    data.put("timeUsage", it.getTimeUsage());
+
+                    metricsTime.add(data);
+                });
+            }
+
+            List<Map<String, Object>> metricsIo = new ArrayList<>();
+            if (opts.metricsIo) {
+                metrics.forEach(it -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("writes", it.getWrites());
+                    data.put("reads", it.getReads());
+
+                    metricsIo.add(data);
+                });
+            }
+
+            return new RunView(
+                    workspaceRef,
+                    general,
+                    config,
+                    params,
+                    command,
+                    status,
+                    processes,
+                    stats,
+                    load,
+                    utilization,
+                    metricsMem,
+                    metricsCpu,
+                    metricsTime,
+                    metricsIo
+            );
 
         } catch (ApiException e) {
             if (e.getCode() == 403) {
