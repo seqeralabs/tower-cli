a7bbdb0a59fbc78ad524230d2e2320e59c323ba9
Julio Fernandez <jfernandez74@gmail.com>
2021-11-08 13:21:12 +0100

    feat: Primary compute env operations
    Set and get a workspace primary compute environent

---
+++ src/main/java/io/seqera/tower/cli/responses/ComputeEnvs/ComputeEnvsPrimaryGet.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021, Seqera Labs.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ */
+
+package io.seqera.tower.cli.responses.ComputeEnvs;
+
+import io.seqera.tower.cli.responses.Response;
+import io.seqera.tower.model.ComputeEnv;
+
+public class ComputeEnvsPrimaryGet extends Response {
+
+    public final String workspaceRef;
+    public final ComputeEnv computeEnv;
+
+    public ComputeEnvsPrimaryGet(String workspaceRef, ComputeEnv computeEnv) {
+        this.workspaceRef = workspaceRef;
+        this.computeEnv = computeEnv;
+    }
+
+    @Override
+    public String toString() {
+        return ansi(String.format("%n  @|yellow Primary compute environment for workspace '%s' is '%s (%s)'  |@%n", workspaceRef, computeEnv.getName(), computeEnv.getId()));
+    }
+}
