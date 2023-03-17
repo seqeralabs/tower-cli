b541b45f9121f0bc562b72f35d33b6be18fc1807
Jaime <jaime.munoz@seqera.io>
2023-03-17 11:15:03 +0100

    list labels cmd

--- src/main/java/io/seqera/tower/cli/commands/labels/ListLabelsCmd.java
+++ src/main/java/io/seqera/tower/cli/commands/labels/ListLabelsCmd.java
@@ -15,20 +15,57 @@ import io.seqera.tower.ApiException;
 import io.seqera.tower.cli.commands.computeenvs.AbstractComputeEnvCmd;
 import io.seqera.tower.cli.commands.computeenvs.primary.GetCmd;
 import io.seqera.tower.cli.commands.computeenvs.primary.SetCmd;
+import io.seqera.tower.cli.commands.global.PaginationOptions;
+import io.seqera.tower.cli.commands.global.WorkspaceOptionalOptions;
 import io.seqera.tower.cli.exceptions.ShowUsageException;
 import io.seqera.tower.cli.responses.Response;
+import io.seqera.tower.model.LabelType;
 import picocli.CommandLine;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 
 @CommandLine.Command(
         name = "list",
         description = "List labels"
 )
-public class ListLabelsCmd extends AbstractComputeEnvCmd {
+public class ListLabelsCmd extends AbstractLabelsCmd {
+
+    @CommandLine.Mixin
+    public WorkspaceOptionalOptions workspaceOptionalOptions;
+
+    @CommandLine.Option(names = {"-t", "--type"}, description = "Label type (normal|resource|all).", defaultValue = "all")
+    public String labelType;
+
+    @CommandLine.Option(names = {"-f", "--filter"}, description = "Show only labels that contain the given word.")
+    @Nullable
+    public String filter;
+
+    @CommandLine.Mixin
+    public PaginationOptions paginationOptions;
 
     @Override
     protected Response exec() throws ApiException, IOException {
-        throw new ShowUsageException(getSpec(), "Missing Required Subcommand");
+
+        Long wspId = workspaceId(workspaceOptionalOptions.workspace);
+
+        Integer max = PaginationOptions.getMax(paginationOptions);
+        Integer offset = PaginationOptions.getOffset(paginationOptions, max);
+
+        LabelType typeEnum = LabelType.ALL;
+        if (labelType == "simple") {
+            typeEnum = LabelType.SIMPLE;
+        } else if (labelType == "resource") {
+            typeEnum = LabelType.RESOURCE;
+        } else if (labelType == "all") {
+            typeEnum = LabelType.ALL;
+        } else {
+            throw new IllegalArgumentException("Label type " + labelType + " is not supported");
+        }
+
+        api().listLabels(wspId, max, offset, filter == null ? "" : filter, typeEnum);
+
+
+        return new GenericStrResponse("lima lmao");
     }
 }
