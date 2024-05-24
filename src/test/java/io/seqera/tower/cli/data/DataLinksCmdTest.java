cfa0828df53dbb6dc15d218aee9f02c546bc18b8
Weronika Sosnowska <weronika.sosnowska@seqera.io>
2024-05-24 13:10:37 +0200

    add test and license comments

--- src/test/java/io/seqera/tower/cli/data/DataLinksCmdTest.java
+++ src/test/java/io/seqera/tower/cli/data/DataLinksCmdTest.java
@@ -26,7 +26,10 @@ import io.seqera.tower.cli.commands.data.links.AbstractDataLinksCmd;
 import io.seqera.tower.cli.commands.enums.OutputType;
 import io.seqera.tower.cli.responses.data.DataLinksList;
 import io.seqera.tower.cli.utils.PaginationInfo;
+import io.seqera.tower.model.DataLinkCreateRequest;
 import io.seqera.tower.model.DataLinkDto;
+import io.seqera.tower.model.DataLinkProvider;
+import io.seqera.tower.model.DataLinkType;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -41,6 +44,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockserver.matchers.Times.exactly;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.JsonBody.json;
 
 public class DataLinksCmdTest extends BaseCmdTest {
 
@@ -241,5 +245,62 @@ public class DataLinksCmdTest extends BaseCmdTest {
         assertEquals(0, out.exitCode);
     }
 
+    @ParameterizedTest
+    @EnumSource(OutputType.class)
+    void testAdd(OutputType format, MockServerClient mock) throws JsonProcessingException {
+        DataLinkCreateRequest req = new DataLinkCreateRequest();
+        req.name("name");
+        req.description("desc");
+        req.type(DataLinkType.BUCKET);
+        req.resourceRef("s3://bucket");
+        req.provider(DataLinkProvider.AWS);
+        req.credentialsId("credsId");
+        req.publicAccessible(false);
 
+        // status check
+        mock.when(
+                request()
+                        .withMethod("POST").withPath("/data-links")
+                        .withQueryStringParameter("workspaceId", "75887156211589")
+                        .withBody(json("{\n" +
+                                "   \"name\":\"name\",\n" +
+                                "   \"description\":\"somedesc\",\n" +
+                                "   \"type\":\"bucket\",\n" +
+                                "   \"provider\":\"aws\",\n" +
+                                "   \"resourceRef\":\"s3://bucket\",\n" +
+                                "   \"publicAccessible\":false,\n" +
+                                "   \"credentialsId\":\"57Ic6reczFn78H1DTaaXkp\"\n" +
+                                "}")),
+                exactly(1)
+        ).respond(
+                response().withStatusCode(200).withBody("{\n" +
+                        "    \"id\": \"v1-user-ad5192871d3d65e1218ec6c4a2f7cde5\",\n" +
+                        "    \"name\": \"name\",\n" +
+                        "    \"description\": \"somedesc\",\n" +
+                        "    \"resourceRef\": \"s3://bucket\",\n" +
+                        "    \"type\": \"bucket\",\n" +
+                        "    \"provider\": \"aws\",\n" +
+                        "    \"region\": \"us-east-1\",\n" +
+                        "    \"credentials\": [\n" +
+                        "        {\n" +
+                        "            \"id\": \"57Ic6reczFn78H1DTaaXkp\",\n" +
+                        "            \"name\": \"aws\",\n" +
+                        "            \"provider\": \"aws\"\n" +
+                        "        }\n" +
+                        "    ],\n" +
+                        "    \"publicAccessible\": false,\n" +
+                        "    \"hidden\": false,\n" +
+                        "    \"status\": null,\n" +
+                        "    \"message\": null\n" +
+                        "}").withContentType(MediaType.APPLICATION_JSON)
+        );
+
+        ExecOut out = exec(format, mock, "data-links", "add", "-w", "75887156211589", "-n", "name", "-d", "somedesc", "-p", "aws",
+                "-u", "s3://bucket", "-c", "57Ic6reczFn78H1DTaaXkp");
+
+        // No errors thrown
+        assertEquals("", out.stdErr);
+        assertEquals(0, out.exitCode);
+    }
+    
 }
