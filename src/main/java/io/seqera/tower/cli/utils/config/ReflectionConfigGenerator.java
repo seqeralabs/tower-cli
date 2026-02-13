cd66cd11663b8c2bccfdaab39f2ce7a2c3d2cc40
Nathan Johnson <nathan.johnson@seqera.io>
2026-02-13 09:06:03 +0000

    feat: fixed runReflectionConfigGenerator to include responses and commands
    Refs:  PLAT-4555

--- src/main/java/io/seqera/tower/cli/utils/config/ReflectionConfigGenerator.java
+++ src/main/java/io/seqera/tower/cli/utils/config/ReflectionConfigGenerator.java
@@ -30,17 +30,19 @@ import java.lang.reflect.*;
  * <a href="https://www.graalvm.org/jdk21/reference-manual/native-image/metadata/AutomaticMetadataCollection/#tracing-agent">...</a>
  *
  * This is a utility class that uses reflection to make calls to all classes in the Tower SDK model package,
- * in order to capture methods reflection metadata of any new fields/classes in Tower SDK.
+ * CLI command classes, and CLI response classes, in order to capture reflection metadata for GraalVM native image.
  * Execute this class with the GraalVM tracing agent (see Gradle task 'runReflectionConfigGenerator').
  *
  */
 public class ReflectionConfigGenerator {
 
     private static final String TOWER_MODEL_PACKAGE = "io.seqera.tower.model";
+    private static final String CLI_COMMANDS_PACKAGE = "io.seqera.tower.cli.commands";
+    private static final String CLI_RESPONSES_PACKAGE = "io.seqera.tower.cli.responses";
 
     public static void main(String[] args) {
         try (ScanResult scanResult = new ClassGraph()
-                .acceptPackages(TOWER_MODEL_PACKAGE)
+                .acceptPackages(TOWER_MODEL_PACKAGE, CLI_COMMANDS_PACKAGE, CLI_RESPONSES_PACKAGE)
                 .enableClassInfo()
                 .scan()
         ) {
